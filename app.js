1749549b0c3dd7768a4b9909a5d9259c05c70558
AGpeter2018 <105569283+AGpeter2018@users.noreply.github.com>
2025-02-02 16:21:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const toggle-botton = document.getElementById("toggle")
-const list = document.getElementById("list-item")
-toggle-botton.addEventListener("click", () => {
-toggle - botton.classList.add(show - links);
-})
+// ********** set date ************
+// select span
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
+
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
