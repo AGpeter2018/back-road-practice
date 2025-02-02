af1489b752a454d90e4d1f672964588037fe00cb
AGpeter2018 <105569283+AGpeter2018@users.noreply.github.com>
2025-02-02 16:18:55 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const toggle-botton = document.getElementById("toggle")
+const list = document.getElementById("list-item")
+toggle-botton.addEventListener("click", () => {
+toggle - botton.classList.add(show - links);
+})
